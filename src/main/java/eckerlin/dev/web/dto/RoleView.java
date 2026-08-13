d01cc28f02c51479cd1ea3fcc3b4618e7b83bcfb
Marco Eckerlin <marcoeckerlin1@gmail.com>
2026-08-13 15:06:57 +0000

    main: Integrationszweig
    Der Anwendungscode liegt ab jetzt in den Zweigen core, ai-radio und
    lavalink. main haelt zusammen, was ueber die Komponenten hinweg gilt:
    Docker-Compose, Dokumentation und Betriebsskripte.

--- src/main/java/eckerlin/dev/web/dto/RoleView.java
+++
@@ -1,7 +0,0 @@
-package eckerlin.dev.web.dto;
-
-public record RoleView(
-        String id,
-        String name
-) {
-}
--- src/main/java/eckerlin/dev/web/dto/VoiceChannelView.java
+++
@@ -1,4 +0,0 @@
-package eckerlin.dev.web.dto;
-
-public record VoiceChannelView(String id, String name, int memberCount, int bitrateKbps) {
-}
